848ce418a8b119c09431d646890621a685c4a6e8
Andrew Shebanow <ashebanow@gmail.com>
2025-01-12 16:00:56 -0800

    RPM-OSTREE refresh-md doesn't work in image builder environment

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ fi
 # Install 1password repo.
 rpm --import https://downloads.1password.com/linux/keys/1password.asc
 echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo
-rpm-ostree refresh-md --force
+# rpm-ostree refresh-md --force
 
 
 #######################################################################
